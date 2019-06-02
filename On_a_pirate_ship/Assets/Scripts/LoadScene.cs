14dd0f63f0005c8a125b4272653a8ba8106f40d3
Karol Kostyra <karol.m.kostyra@gmail.com>
2019-06-02 14:01:56 +0200

    v1
    -Created project
    -Added 'main menu' scene
    -Added 'choose character' scene
    -Added 'play as pirate captain' scene
    -Modified scripts with base functionality

---
+++ On_a_pirate_ship/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadScene : MonoBehaviour
+{
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
---
+++ On_a_pirate_ship/Assets/Scripts/State.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)] [SerializeField] string storyText; //mozna zmienic na public zamiast SerializeField
+    [SerializeField] public State[] nextStates;
+    public Sprite storyImage;
+
+    //[SerializeField] string[] impactOnStats;
+    public DataOfStats[] dataOfStats;
+
+
+    //
+    [System.Serializable]
+    public class DataOfStats
+    {
+        public string nameOfStat;
+        public int valueOfStat;
+    }
+    //
+
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+
+    public Sprite GetStateImage()
+    {
+        return storyImage;
+    }
+
+
+    /*public string[] GetImpactOnStats()
+    {
+        return impactOnStats;
+    }
+    */
+    public DataOfStats[] GetImpactOnStats()
+    {
+        return dataOfStats;
+    }
+}
